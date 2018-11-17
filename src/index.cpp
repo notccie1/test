60b34d2633ceab91da1a1e27b3873e3ce6fc0081
ikuchin <ikuchin@dev.connme.ru>
2018-11-17 00:07:09 -0500

    travis debugging

---
+++ src/index.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Magick++.h>
+using namespace std;
+using namespace Magick;
+int main(int argc, char **argv) {
+  InitializeMagick(*argv);
+  Image image;
+  try {
+    image.read(argv[1]);
+  } 
+  catch( Exception &error_ ) {
+    cout << "Caught exception: " << error_.what() << endl; 
+      return 1; 
+  } 
+  int x = image.columns();
+  cout<<"your picture's width is "<< x << "px"<<endl;
+  return 0; 
+}
